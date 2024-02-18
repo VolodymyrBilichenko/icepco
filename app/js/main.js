3755c517be62f49c1c81114690aefb2e99b0ed98
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2024-02-18 18:11:53 +0200

    up done

--- app/js/main.js
+++ app/js/main.js
@@ -1,31 +1,34 @@
 // input number //
 
-const input = document.querySelector("#phone");
+const inputs = document.querySelectorAll(".phone");
 const output = document.querySelector("#output");
 
-const iti = window.intlTelInput(input, {
-  initialCountry: "us",
-  nationalMode: true,
-  utilsScript: "/intl-tel-input/js/utils.js?1707906286003" // just for formatting/placeholders etc
+inputs.forEach(input => {
+    // Initialize intlTelInput for each phone input
+    const iti = window.intlTelInput(input, {
+        initialCountry: "us",
+        nationalMode: true,
+        utilsScript: "/intl-tel-input/js/utils.js?1707906286003"
+    });
+
+    // Handle input changes and validation
+    const handleChange = () => {
+        let text;
+        if (input.value) {
+            text = iti.isValidNumber()
+                ? "Valid number! Full international format: " + iti.getNumber()
+                : "Invalid number - please try again";
+        } else {
+            text = "Please enter a valid number below";
+        }
+        const textNode = document.createTextNode(text);
+        output.innerHTML = "";
+        output.appendChild(textNode);
+    };
+
+    input.addEventListener("change", handleChange);
+    input.addEventListener("keyup", handleChange);
 });
-
-const handleChange = () => {
-  let text;
-  if (input.value) {
-    text = iti.isValidNumber()
-      ? "Valid number! Full international format: " + iti.getNumber()
-      : "Invalid number - please try again";
-  } else {
-    text = "Please enter a valid number below";
-  }
-  const textNode = document.createTextNode(text);
-  output.innerHTML = "";
-  output.appendChild(textNode);
-};
-
-// listen to "keyup", but also "change" to update when the user selects a country
-input.addEventListener('change', handleChange);
-input.addEventListener('keyup', handleChange);
 
 
 
